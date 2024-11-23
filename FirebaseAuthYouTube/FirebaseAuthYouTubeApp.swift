d10b53c807e0414f77380c385984aa05f5aa508a
Bipul Islam <bipulhstu@gmail.com>
2024-11-23 12:18:31 +0600

    Initial Commit

---
+++ FirebaseAuthYouTube/FirebaseAuthYouTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthYouTubeApp.swift
+//  FirebaseAuthYouTube
+//
+//  Created by Bipul Islam on 23/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthYouTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
